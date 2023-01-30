9ff58305ebfd9f136774c7980e5f12d6d584d88c
Marc <teste74@hotmail.fr>
2023-01-30 18:56:37 +0000

    Update package.sh

--- V2/package.sh
+++ V2/package.sh
@@ -70,9 +70,21 @@ else
 fi
 
 ####################################################################
+# Génération d'ArchISO #
+########################
 if [[ "$GENERATION_ARCHISO" = [yY] ]]; then
   GENERATION_ARCHISO;
   echo "# [Y]  GENERATION ARCHISO  #";
 else
   echo "# [N]  GENERATION ARCHISO  #";
 fi
+
+####################################################################
+if [[ "$PACMAN" = [yY] ]]; then
+  PACMAN;
+  echo "# [Y]  PACMAN              #";
+else
+  echo "# [N]  PACMAN              #";
+fi
+
+
