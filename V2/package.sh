b3016640e6858602ecd3fbe7499700789fd679fc
Marc <teste74@hotmail.fr>
2023-01-30 18:44:58 +0000

    Update package.sh

--- V2/package.sh
+++ V2/package.sh
@@ -22,14 +22,14 @@ FORM;
 # Configuration de la Purge #
 #############################
 if [[ "$PURGE_ALL" = [yY] ]]; then
-  PURGE;
+  PURGE_ALL;
   echo "# [Y]  PURGE_ALL    [OK]   #";
 else
   echo "# [N]  PURGE_ALL    [OK]   #";
 fi
 
 if [[ "$PURGE_ISO" = [yY] ]]; then
-  PURGE;
+  PURGE_ISO;
   echo "# [Y]  PURGE_ISO    [OK]   #";
 else
   echo "# [N]  PURGE_ISO    [OK]   #";
@@ -37,21 +37,21 @@ fi
 
 
 if [[ "$PURGE_PROFIL" = [yY] ]]; then
-  PURGE;
+  PURGE_PROFIL;
   echo "# [Y]  PURGE_PROFIL [OK]   #";
 else
   echo "# [N]  PURGE_PROFIL [OK]   #";
 fi
 
 if [[ "$PURGE_REPO" = [yY] ]]; then
-  PURGE;
+  PURGE_REPO;
   echo "# [Y]  PURGE_REPO   [OK]   #";
 else
   echo "# [N]  PURGE_REPO   [OK]   #";
 fi
 
 if [[ "$PURGE_WORK" = [yY] ]]; then
-  PURGE;
+  PURGE_WORK;
   echo "# [Y]  PURGE_WORK   [OK]   #";
 else
   echo "# [N]  PURGE_WORK   [OK]   #";
