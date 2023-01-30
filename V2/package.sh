62c1f82c5481de9fd8039359118734d8607939fd
Marc <teste74@hotmail.fr>
2023-01-30 18:35:26 +0000

    Update package.sh

--- V2/package.sh
+++ V2/package.sh
@@ -1,1 +1,19 @@
+####################################################################
+# Nettoyage de la console #
+###########################
+clear;
 
+####################################################################
+# Chargement des fonctions #
+############################
+source ./fonctions/packages/list/experimental;
+source ./fonctions/packages/list/stable;
+source ./fonctions/packages/generic;
+source ./fonctions/experimental;
+source ./fonctions/base;
+source ./settings.conf;
+
+####################################################################
+# Mise en Forme #
+#################
+FORM;
