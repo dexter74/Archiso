46123863f9422208e047dbcc57ab1b6262321c6d
Marc <teste74@hotmail.fr>
2023-02-01 16:54:13 +0000

    Update 05_List_Package.sh

--- V3/05_List_Package.sh
+++ V3/05_List_Package.sh
@@ -12,25 +12,25 @@ echo "# Démarrage du script d'ajout de package                  #";
 
 if [[ $LIST_BASE = [yY] ]]; then
 echo "# - Les packages de bases ont étés ajoutées               #";
-package_base;
+#package_base;
 fi
 
 if [[ $LIST_YAY = [yY] ]]; then
 echo "# - Le Package YAY a été ajouté                           #";
-package_yay;
+#package_yay;
 fi
 
-#if [[ $LIST_CALAMARES = [yY] ]]; then
-#echo "# - Le Package Calamares a été ajouté                     #";
+if [[ $LIST_CALAMARES = [yY] ]]; then
+echo "# - Le Package Calamares a été ajouté                     #";
 #package_calamares;
-#fi
+fi
 
-#if [[ $LIST_MICROSOFT_EDGE = [yY] ]]; then
-#echo "# - Le Package Microsoft Edge                             #";
+if [[ $LIST_MICROSOFT_EDGE = [yY] ]]; then
+echo "# - Le Package Microsoft Edge                             #";
 #package_microsoft;
-#fi
+fi
 
-#if [[ $LIST_PILOTE = [yY] ]]; then
-#echo "# - Le Package Pilote Drthrax a été ajouté                #";
+if [[ $LIST_PILOTE = [yY] ]]; then
+echo "# - Le Package Pilote Drthrax a été ajouté                #";
 #package_drthrax;
 #fi
