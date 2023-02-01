7b1645efd9bddab02b75f2f841c7155e9418b45f
Marc <teste74@hotmail.fr>
2023-02-01 15:00:02 +0000

    Update 13_Clean_OS.sh

--- V3/13_Clean_OS.sh
+++ V3/13_Clean_OS.sh
@@ -5,8 +5,8 @@ source ./settings.conf;
 source ./fonctions/clean_os;
 
 ################################################################################################################################################
-# Lancement des Fonctions #
-###########################
+# Lancement de la Fonction #
+############################
 echo "Démarrage du script de nettoyage du Système d'exploitation. (Work, ISO)";
 CLEAN_ISO;
 CLEAN_WORK;
