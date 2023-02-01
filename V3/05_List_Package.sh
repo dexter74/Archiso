5646334bd113c45df52d82f534ed6dcd1ca5664e
Marc <teste74@hotmail.fr>
2023-02-01 15:26:12 +0000

    Update 05_List_Package.sh

--- V3/05_List_Package.sh
+++ V3/05_List_Package.sh
@@ -7,26 +7,24 @@ source ./fonctions/list_package;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-echo "Démarrage du script d'ajout de package.";
+echo "# Démarrage du script d'ajout de package                  #";
 
 if [[ $LIST_BASE = [yY] ]]; then
-  echo "Les packages de bases ont étés ajoutées";
+  echo "# - Les packages de bases ont étés ajoutées             #";
   package_base;
 fi
 
 if [[ $LIST_YAY = [yY] ]]; then
-  echo "Le Package YAY a été ajouté";
+  echo "# - Le Package YAY a été ajouté                         #";
   package_yay;
 fi
 
 if [[ $LIST_CALAMARES = [yY] ]]; then
-  echo "Le Package Calamares a été ajouté";
+  echo "# - Le Package Calamares a été ajouté                   #";
   package_calamares;
 fi
 
 if [[ $LIST_PILOTE = [yY] ]]; then
-  echo "Le Package Pilote Drthrax a été ajouté";
+  echo "# - Le Package Pilote Drthrax a été ajouté              #";
   package_drthrax;
 fi
-
-
