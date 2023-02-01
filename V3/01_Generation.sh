43de8ab5b5165fcbbf5421e4ed49fbaf96df6838
Marc <teste74@hotmail.fr>
2023-02-01 15:22:01 +0000

    Update 01_Generation.sh

--- V3/01_Generation.sh
+++ V3/01_Generation.sh
@@ -7,6 +7,6 @@ source ./fonctions/generation;
 ################################################################################################################################################
 # Lancement de la Fonction #
 ############################
-echo "Démarrage du script de Generation.";
+echo "# Démarrage du script de Generation          #";
 GENERATION_DEPOT;
 GENERATION_ARCHISO;
