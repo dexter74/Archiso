5566d6eba5ab9944aaaccffba4ef94c7ac2335b5
Marc <teste74@hotmail.fr>
2023-02-01 11:30:22 +0000

    Update 05_List_Package.sh

--- V3/05_List_Package.sh
+++ V3/05_List_Package.sh
@@ -7,8 +7,22 @@ source ./fonctions/list_package;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-package_base;
-package_drthrax;
-package_yay;
-package_calamares;
+
+if [ $LIST_PACKAGE_1 = [yY] ]]; then
+  package_base;
+fi
+
+
+if [ $LIST_PACKAGE_2 = [yY] ]]; then
+  package_yay;
+fi
+
+if [ $LIST_PACKAGE_2 = [yY] ]]; then
+  package_calamares;
+fi
+
+if [ $LIST_PACKAGE_4 = [yY] ]]; then
+  package_drthrax;
+fi
+
 
