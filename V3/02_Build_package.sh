ed34700fce52ae91f46940085dce8b65065609d6
Marc <teste74@hotmail.fr>
2023-02-01 15:23:15 +0000

    Update 02_Build_package.sh

--- V3/02_Build_package.sh
+++ V3/02_Build_package.sh
@@ -7,14 +7,15 @@ source ./fonctions/build/package;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-echo "Démarrage du script de compilation de package.";
+echo "###########################################################";
+echo "# Démarrage du script de compilation de package           #";
 
 if [[ $BUILD_YAY = [yY] ]]; then
-  echo "- Compilation du package YAY";
+  echo "# - Compilation du package YAY                          #";
   yay;
 fi
 
 if [[ $BUILD_CALAMARES = [yY] ]]; then
-  echo "- Compilation du package Calamares";
+  echo "# - Compilation du package Calamares                    #";
   calamares;
 fi
