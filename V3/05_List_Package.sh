c18869c920e2ccaaef66c2c4654e4ec203d3365e
Marc <teste74@hotmail.fr>
2023-02-01 15:37:18 +0000

    Update 05_List_Package.sh

--- V3/05_List_Package.sh
+++ V3/05_List_Package.sh
@@ -11,21 +11,21 @@ echo "###########################################################";
 echo "# Démarrage du script d'ajout de package                  #";
 
 if [[ $LIST_BASE = [yY] ]]; then
-  echo "# - Les packages de bases ont étés ajoutées             #";
-  package_base;
+echo "# - Les packages de bases ont étés ajoutées               #";
+package_base;
 fi
 
 if [[ $LIST_YAY = [yY] ]]; then
-  echo "# - Le Package YAY a été ajouté                         #";
-  package_yay;
+echo "# - Le Package YAY a été ajouté                           #";
+package_yay;
 fi
 
 if [[ $LIST_CALAMARES = [yY] ]]; then
-  echo "# - Le Package Calamares a été ajouté                   #";
-  package_calamares;
+echo "# - Le Package Calamares a été ajouté                     #";
+package_calamares;
 fi
 
 if [[ $LIST_PILOTE = [yY] ]]; then
-  echo "# - Le Package Pilote Drthrax a été ajouté              #";
-  package_drthrax;
+echo "# - Le Package Pilote Drthrax a été ajouté                #";
+package_drthrax;
 fi
