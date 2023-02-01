aab6f20c4308160f0b307e2ec1eb1ed3f611f74a
Marc <teste74@hotmail.fr>
2023-02-01 14:47:34 +0000

    Update 04_Pacman.sh

--- V3/04_Pacman.sh
+++ V3/04_Pacman.sh
@@ -7,7 +7,7 @@ source ./fonctions/pacman;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-echo "Démarrage du script de l'édition de pacman.conf";
+echo "Démarrage du script de l'édition de pacman.";
 
 if [[ $PACMAN = [Yy] ]]; then
   echo "Edition de Pacman.conf";
