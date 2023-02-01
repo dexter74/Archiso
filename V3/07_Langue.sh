6d34177078cf77d4563e08bc856c08070473690f
Marc <teste74@hotmail.fr>
2023-02-01 14:59:19 +0000

    Update 07_Langue.sh

--- V3/07_Langue.sh
+++ V3/07_Langue.sh
@@ -5,7 +5,7 @@ source ./settings.conf;
 source ./fonctions/langue;
 
 ################################################################################################################################################
-# Lancement des Fonctions #
-###########################
+# Lancement de la Fonction #
+############################
 echo "Démarrage du script Langue.";
 LANGUE;
