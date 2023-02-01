9bcbd685fada7894dd6670077fcfa0429fb9bfc0
Marc <teste74@hotmail.fr>
2023-02-01 14:33:39 +0000

    Update 02_Build_package.sh

--- V3/02_Build_package.sh
+++ V3/02_Build_package.sh
@@ -7,11 +7,12 @@ source ./fonctions/build/package;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-
 if [[ $BUILD_YAY = [yY] ]]; then
-  yay;
+echo "[YAY] Démarrage du script 02_Build_Package.sh"
+yay;
 fi
 
 if [[ $BUILD_CALAMARES = [yY] ]]; then
+echo "[Calamares] Démarrage du script 02_Build_Package.sh" 
   calamares;
 fi
