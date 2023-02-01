37f39932a3fb66badb6dfa1509f7d7e4c5164d93
Marc <teste74@hotmail.fr>
2023-02-01 14:47:48 +0000

    Update 02_Build_package.sh

--- V3/02_Build_package.sh
+++ V3/02_Build_package.sh
@@ -7,7 +7,7 @@ source ./fonctions/build/package;
 ################################################################################################################################################
 # Lancement des Fonctions #
 ###########################
-echo "Démarrage du script de compilation de package";
+echo "Démarrage du script de compilation de package.";
 
 if [[ $BUILD_YAY = [yY] ]]; then
   echo "- Compilation du package YAY";
