3e34a5d31ff76864bb5b9fc38798fcdf54a281e4
Marc <teste74@hotmail.fr>
2023-02-04 19:08:47 +0000

    Update 04_Services.sh

--- V3/04_Services.sh
+++ V3/04_Services.sh
@@ -2,7 +2,7 @@
 # Chargements des fonctions #
 #############################
 source ./settings.conf;
-source ./fonctions/services;
+#source ./fonctions/services;
 
 ################################################################################################################################################
 # Lancement de la fonction #
