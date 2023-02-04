3f408152e72981fe8a7f94053a9c512714165923
Marc <teste74@hotmail.fr>
2023-02-04 18:36:29 +0000

    Update 03_Packages.sh

--- V3/03_Packages.sh
+++ V3/03_Packages.sh
@@ -13,42 +13,4 @@ source ./fonctions/regen_depot;
 if [[ $PACKAGES = [Yy] ]];
 then
 echo "TEST"
-  #################################################
-  PACMAN;
-  #################################################
-  if [[ $BASE = [Yy] ]]; then
-  echo "Base"
-  base;
-  fi
-  #################################################
-  if [[ $CALAMARES = [Yy] ]]; then
-  calamares;
-  fi
-  #################################################
-  if [[ $ICECREAM = [Yy] ]]; then
-  icecream;
-  fi
-  #################################################
-  if [[ $MICROSOFT_EDGE = [Yy] ]]; then
-  microsoft_edge;
-  fi
-  #################################################
-  if [[ $QT5WEBKIT = [Yy] ]]; then
-  qt5webkit;
-  fi
-  #################################################
-  if [[ $YAY = [Yy] ]]; then
-  yay;
-  fi
-  #################################################
-  if [[ $PILOTE = [Yy] ]]; then
-    aic94xx_firmware;
-    ast_firmware;
-    broadcom_wl;
-    linux_firmware_qlogic;
-    upd72020x_fwl;
-    wd719x_firmware;
-  fi
-  #################################################
-  REGEN_DEPOT;
 fi
