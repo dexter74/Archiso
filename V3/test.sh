06a4f06dd3a0bcadaac5297f0c514b3c695e51ea
Marc <teste74@hotmail.fr>
2023-02-04 15:56:34 +0000

    Update test.sh

--- V3/test.sh
+++ V3/test.sh
@@ -4,22 +4,18 @@
 sh ./00_Update_upgrade.sh;
 sh ./01_Clean.sh;
 sh ./02_Generation.sh;
-
 sh ./03_Build_package.sh;
 sh ./04_Build_driver.sh;
 sh ./05_Pacman.sh;
 sh ./06_List_Package.sh;
 sh ./07_Services.sh;
-
 sh ./08_Langue.sh;
 sh ./09_Customize.sh;
 sh ./10_Utilisateur.sh;
 sh ./11_Profildef.sh;
 sh ./12_Skel.sh;
-
 sh ./13_Regen_depot.sh;
 sh ./14_Clean_OS.sh;
 sh ./15_Permission.sh;
-
 sh ./16_Compilation.sh;
 sh ./17_Rename.sh;
