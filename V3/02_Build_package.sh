04bbee8cc1baaa3560b82a43dd79c4e5cfceed93
Marc <teste74@hotmail.fr>
2023-02-04 12:02:11 +0000

    Update 02_Build_package.sh

--- V3/02_Build_package.sh
+++ V3/02_Build_package.sh
@@ -8,10 +8,8 @@ source ./fonctions/build/package;
 # Lancement des Fonctions #
 ###########################
 echo "###########################################################";
-echo "# Démarrage du script de compilation de package           #";
-
+echo "# Démarrage du script de compilation des packages         #";
 if [[ $BUILD_CALAMARES = [yY] ]]; then
-  echo "# - Compilation des packages                              #";
   echo "# > IceCream                                              #";
   icecream;
   echo "# > Qt5-WebKit                                            #";
@@ -23,11 +21,11 @@ if [[ $BUILD_CALAMARES = [yY] ]]; then
 fi
 
 if [[ $BUILD_MICROSOFT_EDGE = [yY] ]]; then
-  echo "# - Compilation du package Microsoft-edge                 #";
+  echo "# > Microsoft-edge                                        #";
   microsoft_edge;
 fi
 
 if [[ $BUILD_YAY = [yY] ]]; then
-  echo "# - Compilation du package YAY                            #";
+  echo "# > Yay                                                   #";
   yay;
 fi
