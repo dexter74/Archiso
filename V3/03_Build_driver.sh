8a0d2223a751e3aa21d4c2ea6e36c806f19b8903
Marc <teste74@hotmail.fr>
2023-02-04 12:30:03 +0000

    Update 03_Build_driver.sh

--- V3/03_Build_driver.sh
+++ V3/03_Build_driver.sh
@@ -12,21 +12,21 @@ echo "# Démarrage du script de compilation des Pilotes:         #";
 if [[ $BUILD_DRIVER = [yY] ]]; then
 ###################################################################
 echo "# > broadcom_wl                                           #";
-broadcom_wl;
+broadcom_wl > $DIR/repo/log/broadcom_wl.log;
 ###################################################################
 echo "# > Aic94xx-firmware                                      #";
-aic94xx_firmware;
+aic94xx_firmware > $DIR/repo/log/aic94xx_firmware.log;
 ###################################################################
 echo "# > Ast-Firmware                                          #";
-ast_firmware;
+ast_firmware > $DIR/repo/log/Ast-firmware.log;
 ###################################################################
 echo "# > Linux-Firmware-Qlogic                                 #";
-linux_firmware_qlogic;
+linux_firmware_qlogic > $DIR/repo/log/Linux-Firmware-Qlogic.log;
 ###################################################################
-echo "# > Upd72020x-FW                                          #";
-upd72020x_fw;
+echo "# > upd72020x-fw                                          #";
+upd72020x_fw > $DIR/repo/log/upd72020x-fw.log;
 ###################################################################
 echo "# > WD719x-Firmware                                       #";
-wd719x_firmware;
+wd719x_firmware > $DIR/repo/log/wd719x-firmware.log;
 ###################################################################
 fi
