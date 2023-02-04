7eefc5a77e1bdaaf9dbfea30db30f97f1047620e
Marc <teste74@hotmail.fr>
2023-02-04 12:03:00 +0000

    Update 03_Build_driver.sh

--- V3/03_Build_driver.sh
+++ V3/03_Build_driver.sh
@@ -8,9 +8,8 @@ source ./fonctions/build/driver;
 # Lancement de la Fonction #
 ############################
 echo "###########################################################";
-echo "# Démarrage du script de compilation pilote               #";
-
+echo "# Démarrage du script de compilation des packages         #";
 if [[ $BUILD_DRIVER = [yY] ]]; then
-echo "# - Compilation des packages Pilotes                      #";
+echo "# > Pilotes                                               #";
 package_driver_drthrax;
 fi
