19c2a79d50e334b73e5b42872c0398c7227c42b6
Marc <teste74@hotmail.fr>
2023-02-04 17:57:55 +0000

    Create Packages.sh

---
+++ V3/Packages.sh
@@ -0,0 +1,28 @@
+################################################################################################################################################
+# Chargements des fonctions #
+#############################
+source ./settings.conf;
+source ./fonctions/build/package;
+source ./fonctions/build/driver;
+source ./fonctions/pacman;
+source ./fonctions/list_package;
+
+################################################################################################################################################
+# Lancements des fonctions #
+############################
+if [[ $PACKAGE = [Yy] ]]; then
+aic94xx_firmware;
+ast_firmware;
+broadcom_wl;
+linux_firmware_qlogic;
+upd72020x_fw;
+wd719x_firmware;
+
+package_base;
+package_yay;
+package_calamares;
+package_microsoft;
+package_pilote;
+
+PACMAN;
+fi
