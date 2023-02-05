8bf8ef8017699cb98055b397e1d31759a9055a00
Marc <teste74@hotmail.fr>
2023-02-05 02:44:42 +0000

    Update test.sh

--- V3/test.sh
+++ V3/test.sh
@@ -1,10 +1,10 @@
 ######################################################################################################
 # Script pour tester toute les étapes manuellements #
 #####################################################
-#sh ./00_Update_upgrade.sh;
-#sh ./01_Clean.sh;
-#sh ./02_Generation.sh;
-#sh ./03_Packages.sh;
+sh ./00_Update_upgrade.sh;
+sh ./01_Clean.sh;
+sh ./02_Generation.sh;
+sh ./03_Packages.sh;
 sh ./04_Services.sh;
 sh ./05_Langue.sh;
 sh ./06_Customize.sh;
@@ -12,6 +12,6 @@ sh ./07_Utilisateur.sh;
 sh ./08_Profildef.sh;
 sh ./09_Skel.sh;
 sh ./11_Clean_OS.sh;
-#sh ./12_Permission.sh;
-#sh ./13_Compilation.sh;
-#sh ./14_Rename.sh;
+sh ./12_Permission.sh;
+sh ./13_Compilation.sh;
+sh ./14_Rename.sh;
