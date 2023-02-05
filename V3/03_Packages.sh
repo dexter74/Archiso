8f63bd4297cda0c07943d41d57141dc41b1338f5
Marc <teste74@hotmail.fr>
2023-02-05 02:27:32 +0000

    Update 03_Packages.sh

--- V3/03_Packages.sh
+++ V3/03_Packages.sh
@@ -11,40 +11,9 @@ source ./fonctions/regen_depot;
 ############################
 #
 #####################################################################
-if [[ $PACKAGES = [Yy] ]];
-then
+if [[ $PACKAGES = [Yy] ]]; then
   PACMAN;
   base;
-  if [[ $INSTALLATEUR = [Yy] ]]; then
-    calamares;
-    icecream;
-    pythonqt;
-    qt5webkit;
-    PACMAN_DEPOT;
-  fi
-  
-  if [ $BUREAUTIQUE = [Yy] ]]; then
-    microsoft_edge;
-    PACMAN_DEPOT;
-  fi
-  
-  if [ $OUTILS = [Yy] ]]; then
-    yay;
-    PACMAN_DEPOT;
-  fi
-  
-  if [ $PILOTE = [Yy] ]]; then
-    aic94xx_firmware;
-    ast_firmware;
-    broadcom_wl;
-    linux_firmware_qlogic;
-    upd72020x_fw;
-    wd719x_firmware;
-    PACMAN_DEPOT;
-  fi
-  
-  
-
 fi
 
 
