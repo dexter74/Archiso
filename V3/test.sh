cf6d0e31309faf5a6d5023ff5722fa24382a1d33
Marc <teste74@hotmail.fr>
2023-02-05 13:15:33 +0000

    Update test.sh

--- V3/test.sh
+++ V3/test.sh
@@ -10,7 +10,7 @@ sh ./05_Langue.sh;
 sh ./06_Customize.sh;
 sh ./07_Utilisateur.sh;
 sh ./08_Profildef.sh;
-#sh ./09_Skel.sh;
+sh ./09_Skel.sh;
 sh ./10_Regen_depot.sh;
 sh ./11_Clean_OS.sh;
 sh ./12_Permission.sh;
