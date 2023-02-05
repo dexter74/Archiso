abf667965a9055eb847d1108811371e0e0052ca2
Marc <teste74@hotmail.fr>
2023-02-05 19:16:00 +0000

    Update 09_Skel.sh

--- V3/09_Skel.sh
+++ V3/09_Skel.sh
@@ -13,14 +13,14 @@ echo "# - [09] Création du profil utilisateur par défaut           #";
 SKEL;
 
 #######################################################################
-echo "#        >  Copie des Fonds d'écran                          #";
-SKEL_background;
+#echo "#        >  Copie des Fonds d'écran                          #";
+#SKEL_background;
 #######################################################################
-echo "#        >  Copie des Icônes                                 #";
-SKEL_icon;
+#echo "#        >  Copie des Icônes                                 #";
+#SKEL_icon;
 #######################################################################
-echo "#        >  Copie des Thèmes                                 #";
-SKEL_theme;
+#echo "#        >  Copie des Thèmes                                 #";
+#SKEL_theme;
 #######################################################################
 #echo "#        >  Autologin (\$HOME/.dmrc)                          #";
 #autologin;
