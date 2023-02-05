0d3c7c1815a360f34a805fe0eb243395a3ddb66a
Marc <teste74@hotmail.fr>
2023-02-05 02:35:23 +0000

    Update 03_Packages.sh

--- V3/03_Packages.sh
+++ V3/03_Packages.sh
@@ -21,33 +21,33 @@ then
 
   if [[ $INSTALLATEUR = [Yy] ]]; then
     echo "#   > Package Installateur                                #";
-    calamares;
-    icecream;
-    pythonqt;
-    qt5webkit;
-    PACMAN_DEPOT;
+    #calamares;
+    #icecream;
+    #pythonqt;
+    #qt5webkit;
+    #PACMAN_DEPOT;
   fi
 
   if [[ $BUREAUTIQUE = [Yy] ]]; then
     echo "#   > Package Bureautique                                 #";
-    microsoft_edge;
+    #microsoft_edge;
   fi
 
   if [[ $OUTILS = [Yy] ]]; then
     echo "#   > Package OUTILS                                      #";
-    yay;
-    PACMAN_DEPOT;
+    #yay;
+    #PACMAN_DEPOT;
   fi
 
   if [[ $PILOTE = [Yy] ]]; then
     echo "#   > Package OUTILS                                      #";
-    aic94xx_firmware;
-    ast_firmware;
-    broadcom_wl;
-    linux_firmware_qlogic;
-    upd72020x_fw;
-    wd719x_firmware;
-    PACMAN_DEPOT;
+    #aic94xx_firmware;
+    #ast_firmware;
+    #broadcom_wl;
+    #linux_firmware_qlogic;
+    #upd72020x_fw;
+    #wd719x_firmware;
+    #PACMAN_DEPOT;
   fi
 
 fi
